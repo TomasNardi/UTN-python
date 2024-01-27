1ec761165e218b4ecccaafee2c386eb484dfbf4d
Tomas Nardi <tomas.nardi@hotmail.com>
2024-01-27 11:52:49 -0300

    listas 1

--- prueba.py
+++ prueba.py
@@ -1,3 +1,13 @@
-print("hola")
-print('prueba')
-print("nuevo mensje")
+lista1 = ["tomas", "loco", "nardi"]
+lista2 = ["tomas", "martin", "nardi"]
+lista3 = [6, 7, 8, 9, 9, 0]
+
+nueva_lista = lista2 + lista1
+
+print(nueva_lista)
+
+print("----" * 5)
+
+nueva_lista.append(lista3)
+
+print(nueva_lista)
